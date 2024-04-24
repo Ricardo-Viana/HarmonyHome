178caf4076763b5a9103f344d77b622043708e28
Ricardo <ricardo.viana18@gmail.com>
2024-04-24 19:22:59 -0300

    Tela de Casas feitas

--- src/pages/Houses.jsx
+++ src/pages/Houses.jsx
@@ -1,8 +1,9 @@
-import { Container, Box, AppBar, Typography, Toolbar, Button, IconButton, ThemeProvider  } from "@mui/material"
-import MenuIcon from '@mui/icons-material/Menu';
+import { Container, Box, AppBar, Typography, Toolbar, Button, IconButton, Paper  } from "@mui/material"
 import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import NavBarBox from "../components/NavBarBox";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import BottomBarBox from "../components/BottomBarBox";
 
 
 function Houses(){
@@ -11,17 +12,23 @@ function Houses(){
         document.body.style.backgroundColor = "white"
     }, [])
 
-    const casas = [{nome: "Casa1", endereco: "Rua Tal" }, {nome: "Casa2", endereco: "Rua Tel" }, {nome: "Casa3", endereco: "Rua Til" }]
+    const casas = [{nome: "Casa1", endereco: "Rua Tal" }, {nome: "Casa2", endereco: "Rua Tel" }, {nome: "Casa3", endereco: "Rua Til" },]
 
     return(
-        <Container sx={{display: "flex", flexDirection: "row"}}>
+        <>
+        <Container sx={{display: "flex", flexDirection: "row", }}>
             <NavBarBox title="TITULO AQUI"/>
-            <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "300px", marginLeft: "30px" , position: "fixed"}}>
+            <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", marginLeft: "30px", position: "absolute", top: "150px"}}>
             {casas.map((casa, index) => (
-                <Typography variant="h5" ><Link > {index + 1}: {casa.nome} ({casa.endereco})</Link> </Typography>
+                <Typography variant="h5" sx={{color: "black"}}>{index + 1}: <Link style={{color: "black"}} >{casa.nome}</Link> ({casa.endereco})</Typography>
             ))}
+                <IconButton aria-label="Add" sx={{color: "secondary.light"}}>
+                <AddCircleIcon /> Nova Casa
+                </IconButton>
             </Box>
         </Container>
+            <BottomBarBox tasks="tarefasPendentes"></BottomBarBox>
+        </>
     )
 }
 
