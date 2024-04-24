8eb1fa5ec357cc41aaa539a8fd58ace38bf14109
Ricardo <ricardo.viana18@gmail.com>
2024-04-24 09:22:14 -0300

    Refeito home com Material UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,40 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+
+const theme = createTheme(
+  {
+    palette: {
+      background:{
+        default: "#F1F1F4",
+      },
+      primary: {
+        main: "#F1F1F4",
+        light: "#FFFFFF",
+        contrastText: "#000000",
+      },
+      secondary:{
+        main: "#33A071",
+        light: "#0CAE26",
+        contrastText: "#FFFFFF",
+      },
+    },
+    typography:{
+      fontFamily: [
+        'Poppins',
+        'Arial',
+      ].join(','),
+    },
+  });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,56 @@
+import { Box, Button, Container } from "@mui/material"
 import harmonyHomeLogo from "../assets/Harmony_Home_Logo.png"
-import "./styles/Home.css"
 import { useNavigate } from "react-router-dom"
 
 
-function Home(){
-
+function HomeWithMUI(){
     const navigate = useNavigate()
 
-    const handleRedirect = (link) => {
-        navigate(link)
-    }
+    return (
+        <Container sx={{display: "flex", flexDirection: "column", color: "primary.main", gap: 7,}}>
+            
+            <Box
+            sx={{display: "flex",
+                justifyContent: "center",
+                height: 150,}} >
+                <img style={{height: "200px" }} className="logo" src={harmonyHomeLogo}/>
+            </Box>
 
-    return(
-        <>
-            <div className="container">
-                <div className="harmonyHomeLogo">
-                    <img className="logo" src={harmonyHomeLogo}/>
-                </div>
-                <div className="buttons">
-                    <button className="redirectButtonBox" onClick={() => handleRedirect("/register")}>Registre-se</button>
-                    <button className="redirectButtonBox" onClick={() => handleRedirect("/login")}>Faça Login</button>
-                </div>
-            </div>
-        </>
+            <Box
+            sx={{display: "flex",
+                flexDirection: "column",
+                gap: 5,}} >
+                <Button sx={{color: "primary.contrastText",
+                fontFamily: "'Poppins', Arial",
+                fontSize: "20px",
+                fontWeight: 400,
+                letterSpacing: "-px",
+                lineHeight: "auto",
+                textAlign: "center",
+                backgroundColor: "primary.light",
+                borderRadius: "50px",
+                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                padding: "0px",}} onClick={() => {
+                    navigate("/register")
+                }}>Registre-se</Button>
+
+                <Button sx={{color: "primary.contrastText",
+                fontFamily: "'Poppins', Arial",
+                fontSize: "20px",
+                fontWeight: 400,
+                letterSpacing: "-px",
+                lineHeight: "auto",
+                textAlign: "center",
+                backgroundColor: "primary.light",
+                borderRadius: "50px",
+                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                padding: "0px",}} onClick={() => {
+                    navigate("/login")
+                }}>Login</Button>
+
+            </Box>
+        </Container>
     )
 }
 
-export default Home
+export default HomeWithMUI
